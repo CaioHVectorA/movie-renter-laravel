83b28f8eedea5725d808a69918aec8fe14fea870
Your Name <caihebatista@gmail.com>
2024-08-25 01:02:23 -0300

    Fix: routing fixes

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MovieRentController;
 
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/create', [UserController::class, 'create']);
-Route::patch('/update/{id}', [UserController::class, 'update']);
-Route::delete('/delete/{id}', [UserController::class, 'delete']);
+Route::get('/user', [UserController::class, 'index']);
+Route::post('/user/create', [UserController::class, 'create']);
+Route::patch('/user/update/{id}', [UserController::class, 'update']);
+Route::delete('/user/delete/{id}', [UserController::class, 'delete']);
+Route::get('/movies', [MovieController::class, 'index']);
+Route::post('/movie/create', [MovieController::class, 'create']);
+Route::patch('/movie/update/{id}', [MovieController::class, 'update']);
+Route::delete('/movie/delete/{id}', [MovieController::class, 'delete']);
+Route::get('/rent/recent', [MovieRentController::class, 'recent']);
+Route::get('/rent/byUser/{userId}', [MovieRentController::class, 'byUser']);
+Route::post('/rent', [MovieRentController::class, 'rent']);
+Route::patch('/rent/return/{id}', [MovieRentController::class, 'return']);
