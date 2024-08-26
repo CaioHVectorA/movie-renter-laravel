625ff986d6014b2fac73d7bb60ee6245b4afcedc
Your Name <caihebatista@gmail.com>
2024-08-26 01:38:46 -0300

    Fix: infos in controller, some routes

--- app/Http/Controllers/MovieRentController.php
+++ app/Http/Controllers/MovieRentController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\MovieRent;
 use App\Models\Movie;
+use DB;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MovieRentController extends Controller {
     public function recent(Request $request) {
         $days = $request->query('days', 7);
-        $movies_rent = MovieRent::where('created_at', '>=', now()->subDays($days))->get();
-        $movies = Movie::whereIn('id', $movies_rent->pluck('movie_id'))->get();
-        return response()->json($movies);
+        $movies_rent = MovieRent::where('rented_at', '>=', now()->subDays($days))->get();
+        $movies = [];
+        $query = "
+        SELECT R.rented_at, M.title, M.genre, M.id as movie_id, U.name as username, U.id as user_id, U.email as user_email 
+        FROM movie_rents R 
+        INNER JOIN movies M ON M.id = R.movie_id 
+        INNER JOIN users U ON U.id = R.user_id
+        WHERE R.rented_at >= ?
+        ";
+        $movies_rent = DB::select($query, [now()->subDays($days)]);
+        return response()->json($movies_rent);
     }
     public function byUser(Request $request) {
         $user = $request->user();
--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\MovieRentController;
 
-Route::get('/user', [UserController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'index']);
 Route::post('/user/create', [UserController::class, 'create']);
-Route::patch('/user/update/{id}', [UserController::class, 'update']);
-Route::delete('/user/delete/{id}', [UserController::class, 'delete']);
-Route::get('/movies', [MovieController::class, 'index']);
-Route::post('/movie/create', [MovieController::class, 'create']);
-Route::patch('/movie/update/{id}', [MovieController::class, 'update']);
-Route::delete('/movie/delete/{id}', [MovieController::class, 'delete']);
-Route::get('/rent/recent', [MovieRentController::class, 'recent']);
-Route::get('/rent/byUser/{userId}', [MovieRentController::class, 'byUser']);
-Route::post('/rent', [MovieRentController::class, 'rent']);
-Route::patch('/rent/return/{id}', [MovieRentController::class, 'return']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->patch('/user/update/', [UserController::class, 'update']);
+Route::middleware('auth:sanctum')->delete('/user/delete/', [UserController::class, 'delete']);
+Route::middleware('auth:sanctum')->get('/movies', [MovieController::class, 'index']);
+Route::middleware('auth:sanctum')->post('/movie/create', [MovieController::class, 'create']);
+Route::middleware('auth:sanctum')->patch('/movie/update/{id}', [MovieController::class, 'update']);
+Route::middleware('auth:sanctum')->delete('/movie/delete/{id}', [MovieController::class, 'delete']);
+Route::middleware('auth:sanctum')->get('/rent/recent', [MovieRentController::class, 'recent']);
+Route::middleware('auth:sanctum')->get('/rent/my-rents/', [MovieRentController::class, 'byUser']);
+Route::middleware('auth:sanctum')->post('/rent', [MovieRentController::class, 'rent']);
+Route::middleware('auth:sanctum')->patch('/rent/return/{id}', [MovieRentController::class, 'return']);
